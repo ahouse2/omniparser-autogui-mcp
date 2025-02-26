fa530c78e4c2a0aca22338df3698c5f237ea3bfc
NON906 <mumeigames@gmail.com>
2025-02-26 13:10:34 +0900

    sseの設定が反映されない問題の修正

--- src/mcp_autogui/__init__.py
+++ src/mcp_autogui/__init__.py
@@ -4,10 +4,10 @@ def main():
     from mcp.server.fastmcp import FastMCP
     from .mcp_autogui_main import mcp_autogui_main
     if 'SSE_HOST' in os.environ:
-        mcp_main = FastMCP("omniparser_mcp", settings={
-            'host': os.environ['SSE_HOST'],
-            'port': os.environ['SSE_PORT'] if 'SSE_PORT' in os.environ else 8000,
-        })
+        mcp_main = FastMCP("omniparser_mcp",
+            host=os.environ['SSE_HOST'],
+            port=os.environ['SSE_PORT'] if 'SSE_PORT' in os.environ else 8000,
+        )
         mcp_autogui_main(mcp_main)
         mcp_main.run('sse')
     else:
