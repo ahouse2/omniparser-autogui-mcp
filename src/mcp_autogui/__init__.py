6580671db0fea8c05b9140f6bc227b518e4bd9bb
NON906 <mumeigames@gmail.com>
2025-02-26 10:32:29 +0900

    sse機能

--- src/mcp_autogui/__init__.py
+++ src/mcp_autogui/__init__.py
@@ -1,7 +1,16 @@
+import os
 
 def main():
     from mcp.server.fastmcp import FastMCP
     from .mcp_autogui_main import mcp_autogui_main
-    mcp_main = FastMCP("omniparser_mcp")
-    mcp_autogui_main(mcp_main)
-    mcp_main.run()
+    if 'SSE_HOST' in os.environ:
+        mcp_main = FastMCP("omniparser_mcp", settings={
+            'host': os.environ['SSE_HOST'],
+            'port': os.environ['SSE_PORT'] if 'SSE_PORT' in os.environ else 8000,
+        })
+        mcp_autogui_main(mcp_main)
+        mcp_main.run('sse')
+    else:
+        mcp_main = FastMCP("omniparser_mcp")
+        mcp_autogui_main(mcp_main)
+        mcp_main.run()
